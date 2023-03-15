3e4baa4e08eb45ec43878e4f57ef976fe6992158
Akinbebije Micheal <michealakinbebije@gmail.com>
2023-03-15 14:44:04 +0100

    new

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const logger = require('./config/logger');
 const userschema = require("./models/user.model.js")
 // const Uploadschema = require("./models/upload.model.js")
 
-
+const port = process.env.PORT || 8080;
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
   logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
-    logger.info(`Listening to port ${config.port} for database`);
+  server = app.listen(port, () => {
+    logger.info(`Server running on port ${port}`);
   });
 });
 
